f4e082d760baf4e381b03e29015e2bfd973c50b5
anantsxn <anant.sxn@gmail.com>
2019-12-17 19:38:06 +0530

    Inheritance codes

--- Inheritance/src/com/company/Main.java
+++ Inheritance/src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Animal animal = new Animal(1,1,5,5,"Animal");
+        Dog dog = new Dog("Yorkie",8,20,2,4,1,20,"long silky");
+
+        dog.eat();
+        dog.walk();
+        dog.run();
     }
 }
